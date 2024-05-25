c4c36ad21afad97b67a98f7c5e931071443c602c
Asad Siddiquee <asadsidn@gmail.com>
2024-05-25 17:30:55 +0200

    up

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -46,8 +46,27 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, contents}, conv) do
+    %{conv | status: 200, resp_body: contents}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 200, resp_body: "No such file"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "Internal Server Error - #{reason}"}
   end
 
   def format_response(conv) do
@@ -122,6 +141,18 @@ IO.puts(response)
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
